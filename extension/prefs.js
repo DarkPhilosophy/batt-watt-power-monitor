ae1ba8a8faa78b75fc6e2cc9310b25ae47bfef67
GitHub Action <action@github.com>
2026-04-14 03:25:35 +0000

    chore: Sync version, badges, and lint status [skip ci]

--- extension/prefs.js
+++ extension/prefs.js
@@ -7,7 +7,7 @@ import GLib from 'gi://GLib';
 
 import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
-const BUILD_DATE = '2026-04-14T03:02:53.825Z';
+const BUILD_DATE = '2026-04-14T03:25:33.606Z';
 const CHANGELOG = `
 STOCK ICON MODE & CHARGING COLOR TUNING
 
